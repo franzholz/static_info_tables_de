ca02fb9dd209451d337d7a27ef05c2a97cf176ea
Kevin Ditscheid <kevin.ditscheid@sgalinski.de>
2019-01-03 19:37:59 +0100

    [TASK] Use call_user_func for ext_localconf and TCA files
    This commit uses an anonymous function in ext_localconf and TCA
    overrides, so the code becomes more clean.

--- Configuration/TCA/Overrides/static_currencies.php
+++ Configuration/TCA/Overrides/static_currencies.php
@@ -1,19 +1,17 @@
 <?php
-defined('TYPO3_MODE') or die();
 
-$tablename = 'static_currencies';
-$additionalFields = array(
-    'cu_name_en' => 'cu_name_de',
-    'cu_sub_name_en' => 'cu_sub_name_de'
-);
-foreach ($additionalFields as $sourceField => $destField) {
-    $additionalColumns = array();
-    $additionalColumns[$destField] = $GLOBALS['TCA'][$tablename]['columns'][$sourceField];
-    $additionalColumns[$destField]['label'] = 'LLL:EXT:static_info_tables_de/Resources/Private/Language/locallang_db.xlf:static_currencies_item.' . $destField;
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($tablename, $additionalColumns);
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($tablename, $destField, '', 'after:' . $sourceField);
-    // Add as search field
-    $GLOBALS['TCA'][$tablename]['ctrl']['searchFields'] .= ',' . $destField;
-}
-unset($additionalColumns);
-unset($additionalFields);
+call_user_func(function($extKey, $table) {
+	$additionalFields = array(
+		'cu_name_en' => 'cu_name_de',
+		'cu_sub_name_en' => 'cu_sub_name_de'
+	);
+	foreach ($additionalFields as $sourceField => $destField) {
+		$additionalColumns = array();
+		$additionalColumns[$destField] = $GLOBALS['TCA'][$table]['columns'][$sourceField];
+		$additionalColumns[$destField]['label'] = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf:static_currencies_item.' . $destField;
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $additionalColumns);
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, $destField, '', 'after:' . $sourceField);
+		// Add as search field
+		$GLOBALS['TCA'][$table]['ctrl']['searchFields'] .= ',' . $destField;
+	}
+}, 'static_info_tables_de', 'static_currencies');
--- Configuration/TCA/Overrides/static_languages.php
+++ Configuration/TCA/Overrides/static_languages.php
@@ -1,18 +1,16 @@
 <?php
-defined('TYPO3_MODE') or die();
 
-$tablename = 'static_languages';
-$additionalFields = array(
-    'lg_name_en' => 'lg_name_de'
-);
-foreach ($additionalFields as $sourceField => $destField) {
-    $additionalColumns = array();
-    $additionalColumns[$destField] = $GLOBALS['TCA'][$tablename]['columns'][$sourceField];
-    $additionalColumns[$destField]['label'] = 'LLL:EXT:static_info_tables_de/Resources/Private/Language/locallang_db.xlf:static_languages_item.' . $destField;
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($tablename, $additionalColumns);
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($tablename, $destField, '', 'after:' . $sourceField);
-    // Add as search field
-    $GLOBALS['TCA'][$tablename]['ctrl']['searchFields'] .= ',' . $destField;
-}
-unset($additionalColumns);
-unset($additionalFields);
+call_user_func(function($extKey, $table) {
+	$additionalFields = array(
+		'lg_name_en' => 'lg_name_de'
+	);
+	foreach ($additionalFields as $sourceField => $destField) {
+		$additionalColumns = array();
+		$additionalColumns[$destField] = $GLOBALS['TCA'][$table]['columns'][$sourceField];
+		$additionalColumns[$destField]['label'] = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf:static_languages_item.' . $destField;
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $additionalColumns);
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, $destField, '', 'after:' . $sourceField);
+		// Add as search field
+		$GLOBALS['TCA'][$table]['ctrl']['searchFields'] .= ',' . $destField;
+	}
+}, 'static_info_tables_de', 'static_languages');
