bc74590e9da5597a64383bd566a15dcbc717169c
Franz Holzinger <holzinger@ttproducts.de>
2020-11-18 18:23:09 +0100

    ChangeLog. only up to TYPO3 10.4 supported

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = Array (
     'author_company' => 'jambage.com',
     'constraints' => array (
         'depends' => array(
-            'typo3' => '8.7.0-10.99.99',
+            'typo3' => '8.7.0-10.4.99',
             'static_info_tables' => '6.7.3-6.99.99',
         ),
         'conflicts' => array(
