55f97d7d967d920cc5115f897ade59c4329083cb
Franz Holzinger <holzinger@ttproducts.de>
2020-11-18 21:07:46 +0100

    -

--- Configuration/TCA/Overrides/static_currencies.php
+++ Configuration/TCA/Overrides/static_currencies.php
@@ -1,7 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(function($extKey, $table) {
+call_user_func(function($extensionKey, $table) {
     $additionalFields = array(
         'cu_name_en' => 'cu_name_de',
         'cu_sub_name_en' => 'cu_sub_name_de'
@@ -9,7 +9,7 @@ call_user_func(function($extKey, $table) {
     foreach ($additionalFields as $sourceField => $destField) {
         $additionalColumns = array();
         $additionalColumns[$destField] = $GLOBALS['TCA'][$table]['columns'][$sourceField];
-        $additionalColumns[$destField]['label'] = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf:static_currencies_item.' . $destField;
+        $additionalColumns[$destField]['label'] = 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:static_currencies_item.' . $destField;
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $additionalColumns);
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, $destField, '', 'after:' . $sourceField);
         // Add as search field
--- Configuration/TCA/Overrides/static_territories.php
+++ Configuration/TCA/Overrides/static_territories.php
@@ -1,14 +1,14 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(function($extKey, $table) {
+call_user_func(function($extensionKey, $table) {
     $additionalFields = array(
         'tr_name_en' => 'tr_name_de'
     );
     foreach ($additionalFields as $sourceField => $destField) {
         $additionalColumns = array();
         $additionalColumns[$destField] = $GLOBALS['TCA'][$table]['columns'][$sourceField];
-        $additionalColumns[$destField]['label'] = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf:static_territories_item.' . $destField;
+        $additionalColumns[$destField]['label'] = 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:static_territories_item.' . $destField;
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $additionalColumns);
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, $destField, '', 'after:' . $sourceField);
         // Add as search field
