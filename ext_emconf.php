bf16609e2ad577ee1212c909883da944ff8b98d8
Franz Holzinger <franz.holzinger@festwein.de>
2021-10-12 10:12:38 +0200

    structure

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = Array (
-    'title' => 'Static Info Tables (de)',
-    'description' => 'German (de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Static Info Tables (de]',
+    'description' => 'German (de] language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
     'version' => '6.7.8',
     'state' => 'stable',
@@ -12,14 +12,14 @@ $EM_CONF[$_EXTKEY] = Array (
     'author' => 'Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
     'author_company' => 'jambage.com',
-    'constraints' => array (
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '8.7.0-10.4.99',
             'static_info_tables' => '6.7.3-6.99.99',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-        ),
-    ),
-);
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
