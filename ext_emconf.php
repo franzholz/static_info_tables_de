c3eefa66ec976880f746a0a1efd3ee9a5765b61c
Franz Holzinger <franz.holzinger@festwein.de>
2021-11-30 20:52:35 +0100

    * compatibility with TYPO3 11 issue #18: No other modifications.

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,8 +14,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.4.99',
-            'static_info_tables' => '6.7.3-6.99.99',
+			'php' => '7.3.0-7.4.99',
+            'typo3' => '8.7.0-11.5.99',
+            'static_info_tables' => '6.7.3-11.5.99',
         ],
         'conflicts' => [
         ],
