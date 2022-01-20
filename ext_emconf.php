46a68f7b0df7aa5b55d6f7b7474aafa3af76e27f
Franz Holzinger <franz.holzinger@festwein.de>
2022-01-20 10:10:39 +0100

    * compatibility with TYPO3 10: Remove file class.ext_update.php * add requirement for the extension addons_em: This is needed for the Extension Manager if the file ext_tables_static+adt.sql has been modified manually by the admin.

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Static Info Tables (de]',
     'description' => 'German (de] language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
-    'version' => '6.7.10',
+    'version' => '6.7.11',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Franz Holzinger',
@@ -12,8 +12,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
             'static_info_tables' => '6.7.3-11.5.99',
+            'addons_em' => '0.5.0-0.5.99',
         ],
         'conflicts' => [
         ],
