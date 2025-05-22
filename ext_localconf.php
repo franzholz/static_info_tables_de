9076cdb1b514c7108993b59051de32393d793850
Franz Holzinger <franz.holzinger@festwein.de>
2025-05-22 21:40:47 +0200

    remove deprecated TypoScript/Extbase/setup.txt

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,3 @@
 <?php
 defined('TYPO3') || die();
 
-call_user_func(function($extensionKey)
-{
-    // Deprecated since 10.0 @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87623-ReplaceConfigpersistenceclassesTyposcriptConfiguration.html
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.txt">'
-    );
-}, 'static_info_tables_de');
-
